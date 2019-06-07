62569fa7348e7095c40e3ad1b8816d452f3ef5d6
Trey Grider <51487921+treygri@users.noreply.github.com>
2019-06-07 15:05:19 -0500

    Add files via upload

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonFirstGrade = new System.Windows.Forms.Button();
+            this.buttonSecondGrade = new System.Windows.Forms.Button();
+            this.buttonThirdGrade = new System.Windows.Forms.Button();
+            this.buttonFourthGrade = new System.Windows.Forms.Button();
+            this.buttonFive = new System.Windows.Forms.Button();
+            this.buttonSixthGrade = new System.Windows.Forms.Button();
+            this.buttonSeventhGrade = new System.Windows.Forms.Button();
+            this.buttonEighthGrade = new System.Windows.Forms.Button();
+            this.buttonNinethGrade = new System.Windows.Forms.Button();
+            this.buttonTenthGrade = new System.Windows.Forms.Button();
+            this.buttonEleventhGrade = new System.Windows.Forms.Button();
+            this.buttonTwelvethGrade = new System.Windows.Forms.Button();
+            this.buttonKinder = new System.Windows.Forms.Button();
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Exit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonFirstGrade
+            // 
+            this.buttonFirstGrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonFirstGrade.Location = new System.Drawing.Point(8, 224);
+            this.buttonFirstGrade.Name = "buttonFirstGrade";
+            this.buttonFirstGrade.Size = new System.Drawing.Size(88, 81);
+            this.buttonFirstGrade.TabIndex = 0;
+            this.buttonFirstGrade.Text = "1st Grade";
+            this.buttonFirstGrade.UseVisualStyleBackColor = true;
+            this.buttonFirstGrade.Click += new System.EventHandler(this.button_click1);
+            // 
+            // buttonSecondGrade
+            // 
+            this.buttonSecondGrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSecondGrade.Location = new System.Drawing.Point(119, 224);
+            this.buttonSecondGrade.Name = "buttonSecondGrade";
+            this.buttonSecondGrade.Size = new System.Drawing.Size(88, 81);
+            this.buttonSecondGrade.TabIndex = 1;
+            this.buttonSecondGrade.Text = "2nd Grade";
+            this.buttonSecondGrade.UseVisualStyleBackColor = true;
+            this.buttonSecondGrade.Click += new System.EventHandler(this.button_click2);
+            // 
+            // buttonThirdGrade
+            // 
+            this.buttonThirdGrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonThirdGrade.Location = new System.Drawing.Point(240, 224);
+            this.buttonThirdGrade.Name = "buttonThirdGrade";
+            this.buttonThirdGrade.Size = new System.Drawing.Size(88, 81);
+            this.buttonThirdGrade.TabIndex = 2;
+            this.buttonThirdGrade.Text = "3rd Grade";
+            this.buttonThirdGrade.UseVisualStyleBackColor = true;
+            this.buttonThirdGrade.Click += new System.EventHandler(this.button_click3);
+            // 
+            // buttonFourthGrade
+            // 
+            this.buttonFourthGrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonFourthGrade.Location = new System.Drawing.Point(356, 224);
+            this.buttonFourthGrade.Name = "buttonFourthGrade";
+            this.buttonFourthGrade.Size = new System.Drawing.Size(88, 81);
+            this.buttonFourthGrade.TabIndex = 3;
+            this.buttonFourthGrade.Text = "4th Grade";
+            this.buttonFourthGrade.UseVisualStyleBackColor = true;
+            this.buttonFourthGrade.Click += new System.EventHandler(this.button_click4);
+            // 
+            // buttonFive
+            // 
+            this.buttonFive.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonFive.Location = new System.Drawing.Point(8, 353);
+            this.buttonFive.Name = "buttonFive";
+            this.buttonFive.Size = new System.Drawing.Size(88, 81);
+            this.buttonFive.TabIndex = 4;
+            this.buttonFive.Text = "5th Grade";
+            this.buttonFive.UseVisualStyleBackColor = true;
+            this.buttonFive.Click += new System.EventHandler(this.button_click5);
+            // 
+            // buttonSixthGrade
+            // 
+            this.buttonSixthGrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSixthGrade.Location = new System.Drawing.Point(119, 353);
+            this.buttonSixthGrade.Name = "buttonSixthGrade";
+            this.buttonSixthGrade.Size = new System.Drawing.Size(88, 81);
+            this.buttonSixthGrade.TabIndex = 5;
+            this.buttonSixthGrade.Text = "6th Grade";
+            this.buttonSixthGrade.UseVisualStyleBackColor = true;
+            this.buttonSixthGrade.Click += new System.EventHandler(this.button_click6);
+            // 
+            // buttonSeventhGrade
+            // 
+            this.buttonSeventhGrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSeventhGrade.Location = new System.Drawing.Point(240, 353);
+            this.buttonSeventhGrade.Name = "buttonSeventhGrade";
+            this.buttonSeventhGrade.Size = new System.Drawing.Size(88, 81);
+            this.buttonSeventhGrade.TabIndex = 6;
+            this.buttonSeventhGrade.Text = "7th Grade";
+            this.buttonSeventhGrade.UseVisualStyleBackColor = true;
+            this.buttonSeventhGrade.Click += new System.EventHandler(this.button_click7);
+            // 
+            // buttonEighthGrade
+            // 
+            this.buttonEighthGrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEighthGrade.Location = new System.Drawing.Point(356, 353);
+            this.buttonEighthGrade.Name = "buttonEighthGrade";
+            this.buttonEighthGrade.Size = new System.Drawing.Size(88, 81);
+            this.buttonEighthGrade.TabIndex = 7;
+            this.buttonEighthGrade.Text = "8th Grade";
+            this.buttonEighthGrade.UseVisualStyleBackColor = true;
+            this.buttonEighthGrade.Click += new System.EventHandler(this.button_click8);
+            // 
+            // buttonNinethGrade
+            // 
+            this.buttonNinethGrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonNinethGrade.Location = new System.Drawing.Point(8, 473);
+            this.buttonNinethGrade.Name = "buttonNinethGrade";
+            this.buttonNinethGrade.Size = new System.Drawing.Size(88, 81);
+            this.buttonNinethGrade.TabIndex = 8;
+            this.buttonNinethGrade.Text = "Freshman";
+            this.buttonNinethGrade.UseVisualStyleBackColor = true;
+            this.buttonNinethGrade.Click += new System.EventHandler(this.button_click9);
+            // 
+            // buttonTenthGrade
+            // 
+            this.buttonTenthGrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTenthGrade.Location = new System.Drawing.Point(119, 473);
+            this.buttonTenthGrade.Name = "buttonTenthGrade";
+            this.buttonTenthGrade.Size = new System.Drawing.Size(88, 81);
+            this.buttonTenthGrade.TabIndex = 9;
+            this.buttonTenthGrade.Text = "Sophmore";
+            this.buttonTenthGrade.UseVisualStyleBackColor = true;
+            this.buttonTenthGrade.Click += new System.EventHandler(this.button_click10);
+            // 
+            // buttonEleventhGrade
+            // 
+            this.buttonEleventhGrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEleventhGrade.Location = new System.Drawing.Point(240, 473);
+            this.buttonEleventhGrade.Name = "buttonEleventhGrade";
+            this.buttonEleventhGrade.Size = new System.Drawing.Size(88, 81);
+            this.buttonEleventhGrade.TabIndex = 10;
+            this.buttonEleventhGrade.Text = "Junior";
+            this.buttonEleventhGrade.UseVisualStyleBackColor = true;
+            this.buttonEleventhGrade.Click += new System.EventHandler(this.button_click11);
+            // 
+            // buttonTwelvethGrade
+            // 
+            this.buttonTwelvethGrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTwelvethGrade.Location = new System.Drawing.Point(356, 473);
+            this.buttonTwelvethGrade.Name = "buttonTwelvethGrade";
+            this.buttonTwelvethGrade.Size = new System.Drawing.Size(88, 81);
+            this.buttonTwelvethGrade.TabIndex = 11;
+            this.buttonTwelvethGrade.Text = "Senior";
+            this.buttonTwelvethGrade.UseVisualStyleBackColor = true;
+            this.buttonTwelvethGrade.Click += new System.EventHandler(this.button_click12);
+            // 
+            // buttonKinder
+            // 
+            this.buttonKinder.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonKinder.Location = new System.Drawing.Point(182, 575);
+            this.buttonKinder.Name = "buttonKinder";
+            this.buttonKinder.Size = new System.Drawing.Size(88, 81);
+            this.buttonKinder.TabIndex = 12;
+            this.buttonKinder.Text = "Kindergarten";
+            this.buttonKinder.UseVisualStyleBackColor = true;
+            this.buttonKinder.Click += new System.EventHandler(this.button_click);
+            // 
+            // textBox_result
+            // 
+            this.textBox_result.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_result.Location = new System.Drawing.Point(8, 188);
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.Size = new System.Drawing.Size(436, 20);
+            this.textBox_result.TabIndex = 13;
+            this.textBox_result.Text = "Please select a grade level...";
+            this.textBox_result.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox_result.TextChanged += new System.EventHandler(this.textBox_result_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(4, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 24);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Graduation Year:";
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.buttonClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClear.Location = new System.Drawing.Point(319, 588);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(125, 52);
+            this.buttonClear.TabIndex = 15;
+            this.buttonClear.Text = "CLEAR";
+            this.buttonClear.UseVisualStyleBackColor = false;
+            this.buttonClear.Click += new System.EventHandler(this.button_clickClear);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(339, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Author: Trey Grider";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(28, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(143, 123);
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(335, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 13);
+            this.textBox1.TabIndex = 18;
+            this.textBox1.Text = "(version 1.2)";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Exit
+            // 
+            this.Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exit.Location = new System.Drawing.Point(28, 613);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(91, 43);
+            this.Exit.TabIndex = 19;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(472, 711);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_result);
+            this.Controls.Add(this.buttonKinder);
+            this.Controls.Add(this.buttonTwelvethGrade);
+            this.Controls.Add(this.buttonEleventhGrade);
+            this.Controls.Add(this.buttonTenthGrade);
+            this.Controls.Add(this.buttonNinethGrade);
+            this.Controls.Add(this.buttonEighthGrade);
+            this.Controls.Add(this.buttonSeventhGrade);
+            this.Controls.Add(this.buttonSixthGrade);
+            this.Controls.Add(this.buttonFive);
+            this.Controls.Add(this.buttonFourthGrade);
+            this.Controls.Add(this.buttonThirdGrade);
+            this.Controls.Add(this.buttonSecondGrade);
+            this.Controls.Add(this.buttonFirstGrade);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Graddy Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFirstGrade;
+        private System.Windows.Forms.Button buttonSecondGrade;
+        private System.Windows.Forms.Button buttonThirdGrade;
+        private System.Windows.Forms.Button buttonFourthGrade;
+        private System.Windows.Forms.Button buttonFive;
+        private System.Windows.Forms.Button buttonSixthGrade;
+        private System.Windows.Forms.Button buttonSeventhGrade;
+        private System.Windows.Forms.Button buttonEighthGrade;
+        private System.Windows.Forms.Button buttonNinethGrade;
+        private System.Windows.Forms.Button buttonTenthGrade;
+        private System.Windows.Forms.Button buttonEleventhGrade;
+        private System.Windows.Forms.Button buttonTwelvethGrade;
+        private System.Windows.Forms.Button buttonKinder;
+        private System.Windows.Forms.TextBox textBox_result;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Exit;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            //Kindergarten//
+            if (textBox_result.Text == "Please select a grade level...")
+                textBox_result.Clear();
+
+            Button button = (Button)sender;
+            textBox_result.Text = textBox_result.Text + "2032";
+        }
+
+        private void textBox_result_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button_click2(object sender, EventArgs e)
+        {
+            //Second Grade//
+            if (textBox_result.Text == "Please select a grade level...")
+                textBox_result.Clear();
+
+            Button button = (Button)sender;
+            textBox_result.Text = textBox_result.Text + "20230";
+        }
+
+        private void button_click1(object sender, EventArgs e)
+        {
+            if (textBox_result.Text == "Please select a grade level...")
+                textBox_result.Clear();
+
+            //First Grade//
+            Button button = (Button)sender;
+            textBox_result.Text = textBox_result.Text + "2031";
+        }
+
+        private void button_click3(object sender, EventArgs e)
+        {
+            if (textBox_result.Text == "Please select a grade level...")
+                textBox_result.Clear();
+
+            //Third Grade//
+            Button button = (Button)sender;
+            textBox_result.Text = textBox_result.Text + "2029";
+        }
+
+        private void button_click4(object sender, EventArgs e)
+        {
+            if (textBox_result.Text == "Please select a grade level...")
+                textBox_result.Clear();
+
+            //Fourth Grade//
+            Button button = (Button)sender;
+            textBox_result.Text = textBox_result.Text + "2028";
+        }
+
+        private void button_click5(object sender, EventArgs e)
+        {
+            if (textBox_result.Text == "Please select a grade level...")
+                textBox_result.Clear();
+
+            //Fifth Grade//
+            Button button = (Button)sender;
+            textBox_result.Text = textBox_result.Text + "2027";
+        }
+
+        private void button_click6(object sender, EventArgs e)
+        {
+            if (textBox_result.Text == "Please select a grade level...")
+                textBox_result.Clear();
+
+            //Sixth Grade//
+            Button button = (Button)sender;
+            textBox_result.Text = textBox_result.Text + "2026";
+        }
+
+        private void button_click7(object sender, EventArgs e)
+        {
+            //Seventh Grade//
+            if (textBox_result.Text == "Welcome to Trey's Graddy Calculator!, please select a grade level...")
+                textBox_result.Clear();
+
+            Button button = (Button)sender;
+            textBox_result.Text = textBox_result.Text + "2025";
+        }
+
+        private void button_click8(object sender, EventArgs e)
+        {
+            //Eighth Grade//
+            if (textBox_result.Text == "Please select a grade level...")
+                textBox_result.Clear();
+
+            Button button = (Button)sender;
+            textBox_result.Text = textBox_result.Text + "2024";
+        }
+
+        private void button_click9(object sender, EventArgs e)
+        {
+            //Freshman//
+            if (textBox_result.Text == "Please select a grade level...")
+                textBox_result.Clear();
+
+            Button button = (Button)sender;
+            textBox_result.Text = textBox_result.Text + "2023";
+        }
+
+        private void button_click10(object sender, EventArgs e)
+        {
+            //Sophmore//
+            if (textBox_result.Text == "Please select a grade level...")
+                textBox_result.Clear();
+
+            Button button = (Button)sender;
+            textBox_result.Text = textBox_result.Text + "2022";
+        }
+
+        private void button_click11(object sender, EventArgs e)
+        {
+            //Junior//
+            if (textBox_result.Text == "Please select a grade level...")
+                textBox_result.Clear();
+
+            Button button = (Button)sender;
+            textBox_result.Text = textBox_result.Text + "2021";
+        }
+
+        private void button_click12(object sender, EventArgs e)
+        {
+            //Senior//
+            if (textBox_result.Text == "Please select a grade level...")
+                textBox_result.Clear();
+
+            Button button = (Button)sender;
+            textBox_result.Text = textBox_result.Text + "2020";
+        }
+
+        private void button_clickClear(object sender, EventArgs e)
+        {
+            textBox_result.Text = "Please select grade level...";
+            textBox_result.Clear();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
